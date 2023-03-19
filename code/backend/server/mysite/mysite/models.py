bf8266831db87e1d84dc816aca3437af8019d2c1
Tamir Banay <banay9329@gmail.com>
2023-03-19 14:38:23 +0200

    makemigrations models

--- code/backend/server/mysite/mysite/models.py
+++ code/backend/server/mysite/mysite/models.py
@@ -10,7 +10,7 @@ class PayWiseUser (models.Model):
     
 class Wallet(models.Model):
     walletID = models.IntegerField(primary_key=True)
-    userID= models.IntegerField(PayWiseUser.userID,on_delete=models.SET_DEFAULT,default="undefined")
+    userID= models.ForeignKey(PayWiseUser,on_delete=models.SET_DEFAULT,default="undefined")
     
 class VoucherCategory(models.Model):
     categoryID = models.AutoField(primary_key=True)
@@ -27,16 +27,16 @@ class StoreType(models.Model):
 class Store(models.Model):
     storeID  = models.AutoField(primary_key=True)       
     name = models.CharField(max_length=30)
-    type = models.CharField(StoreType.typeID,on_delete=models.SET_DEFAULT,default="undefined",max_length=30)
+    type = models.ForeignKey(StoreType,on_delete=models.SET_DEFAULT,default="undefined",max_length=30)
     city = models.CharField(max_length=30)       
     street = models.CharField(max_length=30,  null=True, blank=True)
     houseNumber = models.CharField(max_length=30, null=True, blank=True)
 
 class Vouchers (models.Model):
     voucherID = models.IntegerField(primary_key=True)       
-    walletID = models.IntegerField(Wallet.walletID,on_delete=models.SET_DEFAULT,default="undefined",primary_key=True,)
-    voucherCategory = models.ForeignKey(VoucherCategory.categoryID,on_delete=models.SET_DEFAULT, default="undefined")
-    storeType = models.ForeignKey(StoreType.typeID,on_delete=models.SET_DEFAULT,default="undefined")
+    walletID = models.ForeignKey(Wallet,on_delete=models.SET_DEFAULT,default="undefined")
+    voucherCategory = models.ForeignKey(VoucherCategory,on_delete=models.SET_DEFAULT, default="undefined")
+    storeType = models.ForeignKey(StoreType,on_delete=models.SET_DEFAULT,default="undefined")
     ammount = models.IntegerField()
     dateOfAcquire = models.DateField()    
     dateOfExpiry = models.DateField()    
@@ -44,8 +44,8 @@ class Vouchers (models.Model):
     
 class Alerts (models.Model):
    alertID = models.IntegerField(primary_key=True)
-   voucherID = models.IntegerField(Vouchers.voucherID,on_delete=models.SET_DEFAULT,default="undefined",primary_key=True,) 
-   walletID = models.IntegerField(Vouchers.walletID,on_delete=models.SET_DEFAULT,default="undefined",primary_key=True,)    
+   voucherID = models.ForeignKey(Vouchers,on_delete=models.SET_DEFAULT,default="undefined") 
+   walletID = models.ForeignKey(Wallet,on_delete=models.SET_DEFAULT,default="undefined")    
    alertDate = models.DateField()
    aletHour  = models.TimeField()
    
