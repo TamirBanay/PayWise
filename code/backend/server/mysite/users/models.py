b401648d267567c94042e4f78a3ef73a4f369c54
Tamir Banay <banay9329@gmail.com>
2023-03-23 11:59:24 +0200

    create users app

--- code/backend/server/mysite/users/models.py
+++ code/backend/server/mysite/users/models.py
@@ -1,23 +1,3 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-
-
-class CustomUser(AbstractUser):
-
-    STATUS = (
-        ('regular', 'regular'),
-        ('subscriber', 'subscriber'),
-        ('moderator', 'moderator')
-    )
-
-
-emil = models.EmailField(unique=True)
-status = models.CharField(max_length=100, choices = 'STATUS' , default='regular')
-decraption = models.TextField(
-    'Decraption', max_length=600, default='', blank=True)
-
-
-def __str__(self):
-    return self.username
