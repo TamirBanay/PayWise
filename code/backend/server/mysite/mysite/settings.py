76a9e54515f73f883309cc1cecbfff8e35c354ff
Tamir Banay <banay9329@gmail.com>
2023-03-23 12:08:19 +0200

    makemigrations users

--- code/backend/server/mysite/mysite/settings.py
+++ code/backend/server/mysite/mysite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'mysite',
     'users',
 ]
-AUTH_USER_MODEL ='users.CustomUser'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -129,3 +129,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
