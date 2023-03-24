634760663b6dadbf8b801783342e01c6cb71ff65
OzBena <ozbenacot@gmail.com>
2023-03-24 16:08:35 +0300

    login and singup

--- code/frontend/zeeksystem/src/pages/Home.js
+++ code/frontend/zeeksystem/src/pages/Home.js
@@ -1,14 +1,27 @@
-import React from "react";
-import { Button,  } from '@mui/material';
-import Link from '@mui/material/Link';
+import React, { useEffect } from "react";
+import { Button } from "@mui/material";
+import Link from "@mui/material/Link";
 
 function Home() {
+  useEffect(() => {
+    (
+       async () => {
+      const response = await fetch("http://localhost:8000/api/user", {
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+      const content = await response.json();
+
+      console.log(content);
+    })();
+  });
+
   return (
     <div>
       <h1>welcome home</h1>
-      <Link href="/signin" variant="body2">
-              <Button variant="contained">Sign me in</Button>
-            </Link>
+      <Link href="/login" variant="body2">
+        <Button variant="contained">Sign me in</Button>
+      </Link>
     </div>
   );
 }
