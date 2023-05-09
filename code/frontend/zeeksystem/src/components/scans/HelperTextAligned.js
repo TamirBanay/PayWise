63d69be161a79f9c3a8b948a5b2538625589fd0a
OzBena <ozbenacot@gmail.com>
2023-05-09 12:16:02 +0300

    connect the vouchers to the mock db

--- code/frontend/zeeksystem/src/components/scans/HelperTextAligned.js
+++ code/frontend/zeeksystem/src/components/scans/HelperTextAligned.js
@@ -2,59 +2,64 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button"; // Import Button from Material-UI
-import { useState } from "react";
-import {_User} from "../../services/atom"
+import { useEffect, useState } from "react";
+import { _User } from "../../services/atom";
 import { useRecoilState } from "recoil";
 
-
 export default function HelperTextAligned(props) {
-  const [sirialNumber, setSirialNumber] = useState();
-  const [user, setUser] = useRecoilState(_User) 
+  const [serialNumber, setSerialNumber] = useState();
+  const [user, setUser] = useRecoilState(_User);
   const [allVouchers, setAllVouchers] = useState();
 
   const getAllVouchers = async () => {
     try {
-      const response = await fetch(
-        `http://localhost:8000/api/getAllVouchers/`
-      );
+      const response = await fetch(`http://localhost:8000/api/getAllVouchers/`);
       const data = await response.json();
-      setAllVouchers(data)
+      const allVouchersArray = JSON.parse(data.MOCK_vouchers);
+      setAllVouchers(allVouchersArray);
     } catch (error) {
       console.error("Error retrieving all vouchers:", error);
     }
   };
- 
- 
- 
+
   const handleSaveVoucher = async (event) => {
-    event.preventDefault();
-    await fetch("http://localhost:8000/api/createVoucher/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        voucherID: sirialNumber,
-        walletID: 1000 + user.id,
-        voucherCategory: "Category A",
-        storeType: "Store B",
-        ammount: "100",
-        redeemed: false,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // console.log("sucsses", data);
-        props.getWallet();
-        props.handleClose();
+    const serialNumberExists = allVouchers.some(
+      (voucher) => voucher.pk == serialNumber
+    );
+    if (!serialNumberExists) {
+      console.log("the voucher is not exist in the system");
+    } else {
+      const voucher = allVouchers.find((voucher) => voucher.pk == serialNumber);
+      event.preventDefault();
+      await fetch("http://localhost:8000/api/createVoucher/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          voucherID: serialNumber,
+          walletID: 1000 + user.id,
+          voucherCategory: voucher.fields.voucherCategory,
+          storeType: voucher.fields.storeType,
+          ammount: voucher.fields.ammount,
+          redeemed: voucher.fields.redeemed,
+        }),
       })
-      .catch((error) => {
+        .then((response) => response.json())
+        .then((data) => {
+          // console.log("sucsses", data);
+          props.getWallet();
+          props.handleClose();
+        });
+      getAllVouchers().catch((error) => {
         console.log("error", error);
       });
+    }
   };
   const handleChange = (e) => {
-    setSirialNumber(e.target.value);
-    getAllVouchers().then(console.log(allVouchers))
+    setSerialNumber(e.target.value);
   };
-  
+  useEffect(() => {
+    getAllVouchers();
+  }, []);
 
   return (
     <Box
