917cb16c4b756f59434930c20cecffdd3827d15f
Aviram Dayan <aviram.dayan@taboola.com>
2023-05-29 13:56:22 +0300

    70-build

--- build/db/schema.sql
+++ build/db/schema.sql
@@ -2,5 +2,5 @@ CREATE SCHEMA IF NOT EXISTS `zeekSchema`;
 
 DROP USER IF EXISTS 'PayWise_db_root'@'%';
 CREATE USER 'PayWise_db_root'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT ALL ON *.* TO 'PayWise_db_root'@'%';
+GRANT ALL ON `zeekSchema`.* TO 'PayWise_db_root'@'%';
 FLUSH PRIVILEGES;
--- code/backend/server/mysite/mysite/urls.py
+++ code/backend/server/mysite/mysite/urls.py
@@ -1,33 +1,21 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from mysite.views import CreateVoucherView, voucher_redeemed,ChangeVoucheralert
-from mysite.models import Vouchers
+from .views import CreateVoucherView, ChangeVoucheralert
+
 from . import views
+
+api_prefix = settings.API_PREFIX
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
-    path('api/createVoucher/', CreateVoucherView.as_view()),
-    path('api/getVouchers/<int:walletID>', views.get_vouchers),
-    path('api/getVouchers/', views.get_all_vouchers),
-    path('api/getAllVouchers/', views.get_MOCK_vouchers),
-    path('api/deletVouchers/<int:voucher_id>', views.delete_voucher),
-    path('api/voucher_redeemed/<int:voucher_id>/', views.voucher_redeemed),
-    path('api/change_days_before_alert/<int:voucher_id>', ChangeVoucheralert.as_view()),
+    path(f'{api_prefix}', include('users.urls')),
+    path(f'{api_prefix}createVoucher/', CreateVoucherView.as_view()),
+    path(f'{api_prefix}getVouchers/<int:walletID>', views.get_vouchers),
+    path(f'{api_prefix}getVouchers/', views.get_all_vouchers),
+    path(f'{api_prefix}getAllVouchers/', views.get_MOCK_vouchers),
+    path(f'{api_prefix}deletVouchers/<int:voucher_id>', views.delete_voucher),
+    path(f'{api_prefix}voucher_redeemed/<int:voucher_id>/', views.voucher_redeemed),
+    path(f'{api_prefix}change_days_before_alert/<int:voucher_id>', ChangeVoucheralert.as_view()),
 
 ]
